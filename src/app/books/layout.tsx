57dc91ff4b329cae1b8134e6d529d0754affc9e4
sarabalteanu <sara.balteanu.03@gmail.com>
2025-12-10 21:17:10 +0200

    Fixed deployment issue again

---
+++ src/app/books/layout.tsx
@@ -0,0 +1,12 @@
+export default function LayoutBook({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <p>Layout pt carti ...</p>
+      {children}
+    </div>
+  );
+}
