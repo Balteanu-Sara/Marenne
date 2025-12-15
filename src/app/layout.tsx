cbef6df5930638a0deaaa96a9e86c133e565274c
sarabalteanu <sara.balteanu.03@gmail.com>
2025-12-15 17:47:38 +0200

    Added 'fonts' folder + Changed and cleaned code structure of main files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Marenne Books",
@@ -24,9 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      {/* <body className={`${geistMono.variable} antialiased`}>{children}</body> */}
+      <body className="m-0 p-0 w-full min-h-screen box-border overflow-x-hidden antialiased">
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/Button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div>
       <p>Doar scris!</p>
       <Button />
     </div>
