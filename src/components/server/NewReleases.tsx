c318203fd7e1f590887fea693fe90f65abd8520f
sarabalteanu <sara.balteanu.03@gmail.com>
2025-12-19 23:58:29 +0200

    Established request for 'New Releases' section the from main page

--- src/components/server/NewReleases.tsx
+++ src/components/server/NewReleases.tsx
@@ -1,3 +1,9 @@
-export default function NewReleases() {
+import { searchBooks } from "@/lib/openLibrary";
+
+export default async function NewReleases() {
+  const newReleases = await searchBooks(
+    "and+OR+the+OR+love&first_publish_year=2025"
+  );
+
   return <section>New Releases</section>;
 }
