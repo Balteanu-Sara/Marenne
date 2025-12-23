809828fc6697d881ae853c907b797f44353f86cb
sarabalteanu <sara.balteanu.03@gmail.com>
2025-12-23 22:12:05 +0200

    Successfully requesting books' data

--- src/lib/openLibrary.ts
+++ src/lib/openLibrary.ts
@@ -7,9 +7,11 @@ async function accessEdition(
   description: string,
   subjects: string[]
 ): Promise<{
+  filledDescription: string;
+  filledSubjects: string[];
   publish_date: string;
   publisher: string;
-  isbn: number;
+  isbn: string;
   pages: number;
 }> {
   const res = await fetch(`${baseUrl + key}/editions.json`);
@@ -17,31 +19,47 @@ async function accessEdition(
   const entries = editionInfo.entries;
 
   let filledDescription: string = description;
-  let filledSubjects: string[] = subjects;
+  let filledSubjects: string[] = [...subjects];
   let publish_date: string = "";
   let publisher: string = "";
-  let isbn: number = -1;
+  let isbn: string = "";
   let pages: number = -1;
 
   for (const entry of entries) {
     if (entry.description && filledDescription === "")
-      filledDescription = entry.description;
+      filledDescription = entry.description
+        ? typeof entry.description === "string"
+          ? entry.description
+          : entry.description.value
+        : "";
     if (entry.subjects && filledSubjects.length === 0)
-      filledSubjects = entry.subjects;
+      filledSubjects = [...entry.subjects[0]];
 
-    if (publish_date !== "" || publisher !== "" || isbn || pages) break;
-    if ("publish_date" in entry) publish_date = entry.publish_date;
-    if ("publishers" in entry) publisher = entry.publishers[0];
-    if ("number_of_pages" in entry) pages = entry.publishers[0];
+    if (!publish_date && "publish_date" in entry)
+      publish_date = entry.publish_date;
+    if (!publisher && "publishers" in entry) publisher = entry.publishers[0];
+    if (pages === -1 && "number_of_pages" in entry)
+      pages = entry.number_of_pages;
+
     if ("isbn_13" in entry || "isbn_10" in entry) {
       isbn = entry.isbn_13 ? entry.isbn_13 : entry.isbn_10;
       if ("publish_date" in entry) publish_date = entry.publish_date;
       if ("publishers" in entry) publisher = entry.publishers[0];
-      if ("number_of_pages" in entry) pages = entry.publishers[0];
+      if ("number_of_pages" in entry) pages = entry.number_of_pages;
     }
+
+    if (publish_date !== "" && publisher !== "" && isbn !== "" && pages > 0)
+      break;
   }
 
-  return { publish_date, publisher, isbn, pages };
+  return {
+    filledDescription,
+    filledSubjects,
+    publish_date,
+    publisher,
+    isbn,
+    pages,
+  };
 }
 
 async function accessWorks(
@@ -56,38 +74,83 @@ async function accessWorks(
       : works.description.value
     : "";
 
-  const subjects: string[] = works.subjects ? works.subjects : [];
+  const subjects: string[] = [];
+
+  if ("subjects" in works) {
+    for (const subject of works.subjects) {
+      if (subjects.length === 3) break;
+      if (subject.includes("(")) continue;
+      const stringSubject =
+        String(subject).charAt(0).toUpperCase() + String(subject).slice(1);
+
+      if (subject.split(", ") === subject) subjects.push(stringSubject);
+      else {
+        subject.split(", ").forEach((sub: string) => {
+          const stringSub =
+            String(sub).charAt(0).toUpperCase() + String(sub).slice(1);
+
+          if (subjects.length < 3) {
+            subjects.push(stringSub);
+          }
+        });
+      }
+    }
+  }
 
   return { description, subjects };
 }
 
-async function clearResult(books: SearchBooksResults): Promise<Book[]> {
-  const searchResult: SearchResult[] = books.docs;
-  const booksDetails: Book[] = [];
-  for (const result of searchResult) {
+export async function clearResult(
+  books: SearchBooksResults,
+  limit: number = 10
+): Promise<Book[]> {
+  const searchResult: SearchResult[] = books.docs.slice(0, limit);
+
+  const bookDetails = searchResult.map(async (result) => {
     const title: string = result.title;
-    const author: string[] = result["author_name"] ? result["author_name"] : [];
+    const author: string = result["author_name"]
+      ? result["author_name"][0]
+      : "";
     const id: string = result.key;
     const cover: number = result.cover_i;
 
-    const { description, subjects } = await accessWorks(id);
-    const { publish_date, publisher, isbn, pages } = await accessEdition(
+    let { description, subjects } = await accessWorks(id);
+    const {
+      filledDescription,
+      filledSubjects,
+      publish_date,
+      publisher,
+      isbn,
+      pages,
+    } = await accessEdition(id, description, subjects);
+    if (description === "") description = filledDescription;
+    if (subjects.length === 0) subjects = [...filledSubjects];
+
+    return {
       id,
+      title,
+      author,
+      cover,
       description,
-      subjects
-    );
-  }
-  return booksDetails;
+      publisher,
+      publish_date,
+      pages,
+      isbn,
+      subjects,
+    };
+  });
+
+  const promisedBooks: Book[] = await Promise.all(bookDetails);
+  return promisedBooks;
 }
 
-export async function searchBooks(query: string): Promise<Book[]> {
-  const res = await fetch(
-    `${baseUrl}/search.json?q=${encodeURIComponent(query)}`,
-    { cache: "force-cache" }
-  );
+export async function searchBooks(query: string): Promise<SearchBooksResults> {
+  const res = await fetch(`${baseUrl}/search.json?q=${query}`, {
+    cache: "force-cache",
+  });
 
   if (!res.ok) throw new Error("Failed to fetch books in searchBooks");
 
   const books: SearchBooksResults = await res.json();
-  return clearResult(books);
+  return books;
 }
