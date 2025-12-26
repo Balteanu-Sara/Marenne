453f08a103bfeebfc8ec3c3c0b038660a1438033
sarabalteanu <sara.balteanu.03@gmail.com>
2025-12-26 15:34:26 +0200

    Refined base of 'NewReleases' component

--- src/components/server/NewReleases.tsx
+++ src/components/server/NewReleases.tsx
@@ -1,24 +1,39 @@
 import { searchBooks, clearResult } from "@/lib/openLibrary";
+import { ViewMore } from "@/components/server/ViewMore";
 import { Book } from "@/types";
+import Image from "next/image";
 
 export default async function NewReleases() {
   const newReleasesDocs = await searchBooks(
     "and+OR+the+OR+love&first_publish_year=2025"
   );
   const newReleases: Book[] = await clearResult(newReleasesDocs);
-  console.log("this is me");
 
   return (
-    <section>
-      {newReleases.map((release) => (
-        <div key={release.id}>
-          <h1>Title: {release.title}</h1>
-          <p>Author: {release.author}</p>
-          <img
-            src={`https://covers.openlibrary.org/b/id/${release.cover}-M.jpg`}
-          />
-        </div>
-      ))}
+    <section className="w-[100%] gap-2">
+      <p className="justify-self-center font-garamond text-[30px]">
+        New Releases
+      </p>
+      <div className="grid grid-cols-3">
+        {newReleases
+          .filter((release, index) => index < 9)
+          .map((release) => (
+            <div key={release.id} className="flex flex-col items-center">
+              <Image
+                src={`https://covers.openlibrary.org/b/id/${release.cover}-M.jpg`}
+                width={180}
+                height={275}
+                alt={`Cover book for ${release.title}`}
+                className="w-[180px] h-[275px]"
+              />
+              <div className="font-courier text-center">
+                <p className="text-clip">{release.title}</p>
+                <p>$20.50</p>
+              </div>
+            </div>
+          ))}
+      </div>
+      <ViewMore href="/" message="releases" />
     </section>
   );
 }
