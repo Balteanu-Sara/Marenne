b2bc6e324573fc83e1abb1b4b5a63282eddbd7e1
sarabalteanu <sara.balteanu.03@gmail.com>
2026-01-08 19:13:11 +0200

    Included Cart in MobileMenu component

--- src/components/client/MobileMenu.tsx
+++ src/components/client/MobileMenu.tsx
@@ -1,23 +1,33 @@
 "use client";
 import { useStateContext } from "@/context/CurrentStateContext";
-import { Search } from "@/components/client/indexClient";
+import { Search, Cart } from "@/components/client/indexClient";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MobileMenu() {
-  const { isMenuOpen, toggleMenu } = useStateContext();
+  const { isMenuOpen, toggleMenu, toggleCart, close } = useStateContext();
+  const pathName = usePathname();
 
   return (
-    <div className="relative top-0 z-10 flex flex-col px-[15px] font-garamond text-[30px] lg:hidden">
+    <div className="relative top-0 z-10 flex flex-col px-[15px] font-garamond text-[30px] h-auto lg:hidden">
       <div className="flex flex-row justify-between">
         <button onClick={toggleMenu}>{isMenuOpen ? "Close" : "Menu"}</button>
-        <p>Cart</p>
+        <button
+          onClick={() => {
+            close();
+            toggleCart();
+          }}
+        >
+          Cart
+        </button>
       </div>
+      <Cart />
       <div
-        className={`flex flex-row justify-between absolute w-screen z-10 bg-gradient-to-b from-[#ffffff] to-[#f2f2f2] top-8 left-0 pl-[15px] pr-[15px] overflow-hidden transition-all duration-500
+        className={`flex flex-row justify-between absolute w-screen z-4 bg-gradient-to-b from-[#ffffff] to-[#f2f2f2] top-8 left-0 pl-[15px] pr-[15px] overflow-hidden transition-all duration-500
           ${isMenuOpen ? "h-[150px] mt-3 mb-3" : "h-0"} `}
       >
         <div className="flex flex-col">
-          <Link href="/" className="h-[35px]">
+          <Link href="/" className="h-[35px] ">
             All
           </Link>
           <Link href="/" className="h-[35px]">
@@ -26,6 +36,7 @@ export default function MobileMenu() {
           <Link href="/" className="h-[35px]">
             Magazines
           </Link>
+          {pathName.endsWith("books") && <p>Filter</p>}
           <Search />
         </div>
         <div className="flex flex-col text-end">
