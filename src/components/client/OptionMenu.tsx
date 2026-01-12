136b10fff1401990d4ad7a0de243678adc9cbcbc
sarabalteanu <sara.balteanu.03@gmail.com>
2026-01-12 18:31:28 +0200

    Created OptionMenu component for code reduction

---
+++ src/components/client/OptionMenu.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function OptionMenu({
+  href,
+  style,
+  click,
+  innerText,
+}: {
+  href: string;
+  style: string | undefined;
+  click: () => void | undefined;
+  innerText: string;
+}) {
+  return (
+    <Link href={href} className={style ? style : ""} onClick={click}>
+      {innerText}
+    </Link>
+  );
+}
--- src/components/client/indexClient.ts
+++ src/components/client/indexClient.ts
@@ -2,4 +2,5 @@ export { default as Search } from "./Search";
 export { default as Carousel } from "./Carousel";
 export { default as MobileMenu } from "./MobileMenu";
 export { default as DesktopMenu } from "./DesktopMenu";
+export { default as OptionMenu } from "./OptionMenu";
 export { default as Cart } from "./Cart";
