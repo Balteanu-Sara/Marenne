162f652a5425712bb359c21afe2609340cacb9cc
sarabalteanu <sara.balteanu.03@gmail.com>
2026-01-12 23:47:30 +0200

    Attached filter menu options to MobileMenu

--- src/components/client/MobileMenu.tsx
+++ src/components/client/MobileMenu.tsx
@@ -1,19 +1,53 @@
 "use client";
 import { useStateContext } from "@/context/CurrentStateContext";
-import { Search, Cart } from "@/components/client/indexClient";
-import Link from "next/link";
+import { Search, Cart, OptionMenu } from "@/components/client/indexClient";
 import { usePathname } from "next/navigation";
 
+const genres: string[] = [
+  "Architecture",
+  "Art",
+  "Children",
+  "Culture",
+  "Design",
+  "Erotic",
+  "Fashion",
+  "Fiction",
+  "Food",
+  "History",
+  "Illustration",
+  "Japanese",
+  "LGBT",
+  "Music",
+  "Nature",
+  "Novelty",
+  "Photography",
+  "Poetry",
+  "Politics",
+  "Science",
+  "Sport",
+  "Technology",
+];
+
 export default function MobileMenu() {
-  const { isMenuOpen, toggleMenu, toggleCart, toggleSearch } =
-    useStateContext();
+  const {
+    isMenuOpen,
+    isFilterOpen,
+    toggleMenu,
+    toggleFilter,
+    toggleCart,
+    toggleSearch,
+  } = useStateContext();
   const pathName = usePathname();
 
   return (
     <div className="relative top-0 z-10 flex flex-col px-[15px] font-garamond text-[30px] h-auto lg:hidden">
       <div className="flex flex-row justify-between">
         <button onClick={toggleMenu}>{isMenuOpen ? "Close" : "Menu"}</button>
-        {pathName.endsWith("books") && <p>Filter</p>}
+        {pathName.includes("books") && (
+          <button onClick={toggleFilter}>
+            {isFilterOpen ? "Close" : "Filter"}
+          </button>
+        )}
         <button onClick={toggleCart}>Cart</button>
       </div>
       <Cart />
@@ -22,27 +56,56 @@ export default function MobileMenu() {
           ${isMenuOpen ? "h-[150px] mt-3 mb-3" : "h-0"} `}
       >
         <div className="flex flex-col">
-          <Link href="/" className="h-[35px]" onClick={toggleMenu}>
-            All
-          </Link>
-          <Link href="/books" className="h-[35px]" onClick={toggleMenu}>
-            Books
-          </Link>
-          <Link href="/" className="h-[35px]" onClick={toggleMenu}>
-            Magazines
-          </Link>
+          <OptionMenu
+            href="/"
+            style="h-[35px]"
+            click={toggleMenu}
+            innerText="All"
+          />
+          <OptionMenu
+            href="/books"
+            style="h-[35px]"
+            click={toggleMenu}
+            innerText="Books"
+          />
+          <OptionMenu
+            href="/magazines"
+            style="h-[35px]"
+            click={toggleMenu}
+            innerText="Magazines"
+          />
           <button onClick={toggleSearch} className="h-[35px] text-left">
             Search
           </button>
         </div>
         <div className="flex flex-col text-end">
-          <Link href="/" className="h-[35px]" onClick={toggleMenu}>
+          <button className="h-[35px]" onClick={toggleMenu}>
             Login
-          </Link>
+          </button>
           <button className="h-[35px]" onClick={toggleMenu}>
             English
           </button>
         </div>
+      </div>
+
+      <div
+        className={`flex flex-col absolute w-screen z-4 bg-gradient-to-b from-[#ffffff] to-[#f2f2f2] top-8 left-0 pl-[15px] pr-[15px] overflow-hidden transition-all duration-400
+          ${isFilterOpen ? "h-[795px] mt-3 mb-3" : "h-0"} `}
+      >
+        <hr />
+        {genres.map((genre, index) => {
+          return (
+            <p key={genre + index}>
+              <OptionMenu
+                href={`books/genre/${genre.toLowerCase()}`}
+                style="block h-[35px]"
+                click={toggleFilter}
+                innerText={genre}
+              />
+            </p>
+          );
+        })}
+        <hr className="mt-[10px]" />
       </div>
       <Search />
     </div>
