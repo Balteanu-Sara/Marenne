706487d567e59483fce63eea3e8ef82812801afd
sarabalteanu <sara.balteanu.03@gmail.com>
2026-01-14 15:53:17 +0200

    Created loading skeleton for suspense fallback

---
+++ src/components/server/LoadingBooks.tsx
@@ -0,0 +1,18 @@
+export default function LoadingBooks() {
+  return (
+    <div className="flex flex-wrap justify-center">
+      {Array.from({ length: 9 }).map((_, i) => {
+        return (
+          <div
+            key={i}
+            className="flex flex-col items-center m-3 w-[180px] gap-3"
+          >
+            <div className="aspect-[3/4] w-[180px] rounded bg-grey-3 animate-pulse" />
+            <div className="h-4 rounded w-full bg-grey-3 animate-pulse" />
+            <div className="h-3 w-1/2 rounded bg-grey-3 animate-pulse" />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
