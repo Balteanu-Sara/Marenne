196ff34a33661f902d3520fba92becf7a24bcdce
sarabalteanu <sara.balteanu.03@gmail.com>
2026-01-20 19:10:02 +0200

    Update loading state of CartButton component

--- src/components/client/CartButton.tsx
+++ src/components/client/CartButton.tsx
@@ -1,22 +1,36 @@
 "use client";
 import { useStateContext } from "@/context/CurrentStateContext";
+import { useState } from "react";
 import { Book } from "@/types";
 
 export default function CartButton({ book }: { book: Book }) {
   const { addToCart } = useStateContext();
+  const [message, setMessage] = useState("Add to Cart");
+
+  function handleClick() {
+    setMessage("Adding...");
+    addToCart({
+      author_name: book.author ? [book.author] : [],
+      cover_i: book.cover,
+      key: book.id,
+      title: book.title,
+    });
+
+    setTimeout(() => {
+      setMessage("Added!");
+    }, 2000);
+    setTimeout(() => {
+      setMessage("Add to Cart");
+    }, 4000);
+  }
+
   return (
     <button
-      className="py-2 text-[30px] lg:opacity-100 lg:transition-opacity lg:duration-300 lg:hover:opacity-50"
-      onClick={() =>
-        addToCart({
-          author_name: book.author ? [book.author] : [],
-          cover_i: book.cover,
-          key: book.id,
-          title: book.title,
-        })
-      }
+      className={`py-2 text-[30px] lg:opacity-100 lg:transition-opacity lg:duration-300 ${message === "Add to Cart" && "lg:hover:opacity-50"}`}
+      onClick={handleClick}
+      disabled={message !== "Add to Cart"}
     >
-      Add to Cart
+      {message}
     </button>
   );
 }
--- src/components/server/BookDetails.tsx
+++ src/components/server/BookDetails.tsx
@@ -47,14 +47,13 @@ export default async function BookDetails({ id }: { id: string }) {
             width={180}
             height={275}
             alt={`Cover book for ${book.title}`}
-            className="self-center w-[180px] h-[275px]"
-            // className="w-auto h-auto border-1 border-black border-solid"
+            className="self-center w-[180px] h-[275px] border-1 border-black border-solid"
           />
         </div>
-        <div className="border-t-black border-t-[1px] border-b-black border-b-[1px] h-auto">
+        <div className="border-t-black border-t-[1px] border-b-black border-b-[1px] h-auto lg:w-50">
           <div className="py-1 border-b-black border-b-[1px]">
             <p className="font-courier uppercase text-sm">Cost</p>
-            <p className="text-[30px] h-11">$10.75</p>
+            <p className="text-[30px] h-11">$10,75</p>
           </div>
           <CartButton book={book} />
         </div>
