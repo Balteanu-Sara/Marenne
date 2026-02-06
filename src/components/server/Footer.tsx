d9009c5e3fb2ed2cc922e39c99704f08659f4092
sarabalteanu <sara.balteanu.03@gmail.com>
2026-02-06 14:12:21 +0200

    Restyle component + Adjust its responsiveness

--- src/components/server/Footer.tsx
+++ src/components/server/Footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 function ShopSection() {
   return (
     <section className="pt-[5px] pb-[8px]">
-      <p className="font-courier text-sm uppercase">Shop</p>
-      <ul className="font-garamond text-2xl">
-        <li className="h-[27px]">
+      <p className="font-courier text-sm uppercase lg:text-lg">Shop</p>
+      <ul className="font-garamond text-2xl lg:text-4xl">
+        <li className="h-[27px] lg:h-[35px]">
           <Link
             href="/"
             className="lg:text-black lg:transition-colors lg:duration-300 lg:hover:text-white"
@@ -13,7 +13,7 @@ function ShopSection() {
             Home
           </Link>
         </li>
-        <li className="h-[27px]">
+        <li className="h-[27px] lg:h-[35px]">
           <Link
             href="/books"
             className="lg:text-black lg:transition-colors lg:duration-300 lg:hover:text-white"
@@ -21,7 +21,7 @@ function ShopSection() {
             Books
           </Link>
         </li>
-        <li className="h-[27px]">
+        <li className="h-[27px] lg:h-[35px]">
           <Link
             href="/magazines"
             className="lg:text-black lg:transition-colors lg:duration-300 lg:hover:text-white"
@@ -29,7 +29,7 @@ function ShopSection() {
             Magazines
           </Link>
         </li>
-        <li className="h-[27px]">
+        <li className="h-[27px] lg:h-[35px]">
           <Link
             href="/"
             className="lg:text-black lg:transition-colors lg:duration-300 lg:hover:text-white"
@@ -45,9 +45,9 @@ function ShopSection() {
 function LinksSection() {
   return (
     <section className="pt-[5px] pb-[8px]">
-      <p className="font-courier text-sm uppercase">Links</p>
-      <ul className="font-garamond text-2xl">
-        <li className="h-[27px]">
+      <p className="font-courier text-sm uppercase lg:text-lg">Links</p>
+      <ul className="font-garamond text-2xl lg:text-4xl">
+        <li className="h-[27px] lg:h-[35px]">
           <Link
             href="/faq"
             className="lg:text-black lg:transition-colors lg:duration-300 lg:hover:text-white"
@@ -55,7 +55,7 @@ function LinksSection() {
             F.A.Q.s
           </Link>
         </li>
-        <li className="h-[27px]">
+        <li className="h-[27px] lg:h-[35px]">
           <a
             href="https://www.instagram.com/antennebooks"
             target="_blank"
@@ -65,7 +65,7 @@ function LinksSection() {
             Instagram
           </a>
         </li>
-        <li className="h-[27px]">
+        <li className="h-[27px] lg:h-[35px]">
           <a
             href="https://www.google.com/maps/place/73+Farringdon+Rd,+London+EC1M+3JQ,+UK/@51.5210262,-0.109002,17z/data=!3m1!4b1!4m6!3m5!1s0x48761b4e1a800293:0x4e19a2a47fe3cba9!8m2!3d51.5210262!4d-0.1064271!16s%2Fg%2F11c3rmm7wg?entry=tts&g_ep=EgoyMDI0MTExMC4wIPu8ASoASAFQAw%3D%3D"
             target="_blank"
@@ -82,8 +82,9 @@ function LinksSection() {
 
 function Logo() {
   return (
-    <div className="font-title text-[17.5vw] lg:text-[18.7vw]">
-      MARENNE BOOKS
+    <div className="font-title flex flex-row justify-around text-[17.7vw] -translate-y-[0.3em] -mb-[0.3em] lg:text-[18.5vw] lg:-translate-y-[0.4em] lg:-mb-[0.5em]">
+      <p>MARENNE</p>
+      <p>BOOKS</p>
     </div>
   );
 }
@@ -97,9 +98,11 @@ export default function Footer() {
       <LinksSection />
       <hr />
       <Logo />
-      <div className="font-courier justify-self-center">
-        <p>Copyright © 2026 MARENNE BOOKS</p>
-        <p className="text-sm text-center">
+      <div className="font-courier text-center">
+        <p className="text-center text-sm lg:text-xl">
+          Copyright © 2026 MARENNE BOOKS
+        </p>
+        <p className="text-xs lg:text-lg text-center">
           Powered and secured by{" "}
           <a href="https://vercel.com/" target="_blank">
             Vercel
