268d816c14221a2f7e27dbebff35fe8d8185cc07
sarabalteanu <sara.balteanu.03@gmail.com>
2026-02-10 18:33:20 +0200

    Add 'About Us' page

---
+++ src/app/about-us/page.tsx
@@ -0,0 +1,30 @@
+export default function AboutUs() {
+  return (
+    <main className="p-[15px] w-[100%] mt-4 mx-auto max-w-[760px] min-h-[410px]">
+      <p className="text-center font-garamond text-3xl lg:text-5xl pb-[20px]">
+        About Us
+      </p>
+
+      <p className="font-garamond text-xl">
+        Marenne Books is a distributor for independent publishers. Established
+        in London in 2010, Marenne Books distributes publications on art,
+        photography, design, illustration, theory, writing, fashion and culture.
+      </p>
+      <br />
+      <p className="font-garamond text-xl pb-[20px]">
+        In the last decade, independent publishing rapidly grew, with many
+        emerging publishers utilising new technologies and the ability to
+        produce books on small scales and at low costs. Marenne Books was
+        realised as a distribution platform against this background, initiated
+        to support the large growth of these emerging publishers, who at the
+        time had limited means to distribute their work using traditional models
+        of book distribution. Marenne Books began by working with publishers Aki
+        Books, Hassla, Nieves, Seems and The Institute of Social Hypocrisy,
+        introducing retailers to publications that had, until then, rarely been
+        available in Europe. Marenne Books now represents publishers from the
+        UK, Europe, USA and Japan and distributes to a wide selection of
+        bookshops and specialist retailers worldwide.
+      </p>
+    </main>
+  );
+}
